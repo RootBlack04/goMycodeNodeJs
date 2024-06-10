6ddd4225b96e540a16dede64c19dc100592d7111
Abdallah Torkamani <abdellahturkmani@gmail.com>
2024-06-10 20:14:48 +0100

    push

--- Model/connection2.js
+++ Model/connection2.js
@@ -6,6 +6,7 @@ dotenv.config({ path: "../.env" });
 
 const mongoDb = process.env.URLMONGODB;
 const port = process.env.PORT;
+console.log("test : " +port);
 
 // .connect("mongodb://127.0.0.1:27017/gomycodeBackend")
 mongoose
